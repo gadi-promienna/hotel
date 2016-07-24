86d7fca26796fe4d901f6032845d3f12522f6e4e
gadi-promienna <gadi@gadi.org.pl>
2016-07-24 16:47:25 +0200

    Dodanie przykładowych pokoi

--- app/db/migrate/20160722094607_add_initial_rooms.rb
+++ app/db/migrate/20160722094607_add_initial_rooms.rb
@@ -1,4 +1,4 @@
-#Wylosowanie pokoi
+#Wylosowanie dwudziestu losowych pokoi.
 class AddInitialRooms < ActiveRecord::Migration
   def up
 	
@@ -15,5 +15,48 @@ class AddInitialRooms < ActiveRecord::Migration
 	end
 	Room.create(floor: "4", standard: "#{stand}", places: "#{places}")
 	end
+
+		5.times do 
+	stand = Random.rand(3)+1
+	places = Random.rand(4)+1
+	case stand
+	 when 1
+	  stand = "normalny"
+	 when 2
+	  stand = "podwyższony"
+  	 when 3
+	  stand = "luksusowy"
+	end
+	Room.create(floor: "3", standard: "#{stand}", places: "#{places}")
+	end
+
+		5.times do 
+	stand = Random.rand(3)+1
+	places = Random.rand(4)+1
+	case stand
+	 when 1
+	  stand = "normalny"
+	 when 2
+	  stand = "podwyższony"
+  	 when 3
+	  stand = "luksusowy"
+	end
+	Room.create(floor: "2", standard: "#{stand}", places: "#{places}")
+	end
+
+		5.times do 
+	stand = Random.rand(3)+1
+	places = Random.rand(4)+1
+	case stand
+	 when 1
+	  stand = "normalny"
+	 when 2
+	  stand = "podwyższony"
+  	 when 3
+	  stand = "luksusowy"
+	end
+	Room.create(floor: "1", standard: "#{stand}", places: "#{places}")
+	end
+
  end
 end
